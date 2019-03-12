bb5e2081ec0e3915d43553e699dc7548326a0cfd
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 19:16:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,3 +21,11 @@ def take_a_number(line, new_person)
 end
 
 
+def now_serving(line)
+  if line.length == 0 
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
+end
