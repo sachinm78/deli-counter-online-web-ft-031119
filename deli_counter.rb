533746d96b13d7b5a57367fe0615a095cbbdc6d2
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 19:12:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
 # Write your code here.
 
-def line_simple(array) 
-  current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-  # "each.with_index" is the method...must use "index"
-    current_line << " #{indexemus}. #{value},"    
-    # "indexemus" is used to illustrate this variable can be different from "index"
-  end 
-  puts current_line
-end 
+def line(array) 
+  if array.length >= 1
+    arr = []
+    counter = 1 
+    array.each do |name|
+      arr.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{arr.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
