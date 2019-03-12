7c29f32173b1e8899a5393aacd6e68d8e0f801c8
Sachin Mahajan <sachinm78@gmail.com>
2019-03-12 19:14:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,9 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+
+def take_a_number(line, new_person)
+  line << new_person
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
